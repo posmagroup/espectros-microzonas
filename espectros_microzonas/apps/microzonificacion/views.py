fd0c8a622dbcd9fc11cff94d3dc3c4a4537da949
Luis Alberto Santana <jackboot7@gmail.com>
2012-11-09 13:37:36 -0430

    Added ProxyHostView to microzones/views/py

--- espectros_microzonas/apps/microzonificacion/views.py
+++ espectros_microzonas/apps/microzonificacion/views.py
@@ -1,19 +1,48 @@
 # Create your views here.
-from django.views.generic import DetailView
+from django.views.generic import DetailView, TemplateView
 
 from braces.views import JSONResponseMixin
 
 
-class TestObject():
-    def __init__(self, name, msg):
-        self.name = name
-        self.msg = msg
+import requests
+
+
+GEOSERVER_URL = 'http://190.200.214.201:8080/geoserver/microzonas/wms'
+
+
+class ProxyHost(TemplateView):
+    """
+    ProxyHost: Implements a proxy to between the OpenLayers and GeoServer, due to
+    HttpRequest limitations.
+
+    Based on the terrible documented code from OpenLayers examples:
+    http://trac.osgeo.org/openlayers/browser/trunk/openlayers/examples/proxy.cgi
+
+    """
+
+    """
+    """
+    def get(self, request, *args, **kwargs):
+
+        payload = {
+            'layers': u'microzonas:Microzonas_Amenaza_General',
+            'request': u'GetFeatureInfo',
+            'version': u'1.1.0',
+            'service': u'WMS',
+        }
+
+        response = requests.get(GEOSERVER_URL, params=payload)
+
+        #print response.__dict__
+        #print response.__dict__['url']
+
+        super(ProxyHost, self).get(request, *args, **kwargs)
 
 
 class MicrozoneDetail(JSONResponseMixin, DetailView):
     """
     Returns a JSON object containing the information from certain microzone.
-    
+
     """
 
     #model = MicrozoneModel
@@ -25,3 +54,4 @@ class MicrozoneDetail(JSONResponseMixin, DetailView):
         }
 
         return self.render_json_response(context_dict)
+
