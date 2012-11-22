fb71586db8c68ce668e83b01c9505beda303aa52
Carlos Bruguera <cbruguera@gmail.com>
2012-11-22 16:52:37 -0430

    inclusión de la biblioteca de jqplot para graficar

--- espectros_microzonas/static/js/main.js
+++ espectros_microzonas/static/js/main.js
@@ -125,7 +125,11 @@ window.mapping = {
 
 
 // window.charts namespace for the chart-related functions.
-window.charts = {  }
+window.charts = {
+    plot_test: function(){
+        $.jqplot('chartdiv',  [[[1, 2],[3,5.12],[5,13.1],[7,33.6],[9,85.9],[11,219.9]]]);
+    }
+}
 
 
 // global functions. 
