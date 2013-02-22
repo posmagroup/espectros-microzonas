1dcb895fdfdc80b799b3ce1952d710c83c035b67
Jesús Gómez <jgomo3@gmail.com>
2013-02-21 23:22:28 -0430

    Habilita django-javascript-settings y prepara el camino para fvislib
    Con django-javascript-settings ya no se mantiene la dirección del
    servidor goegráfico en Python y en javascript, ahora solo se define
    en los settings del proyecto, una sola vez.
    
    Dado que la url del servidor Geográfico pueda ser diferente desde
    el punto de vista del cliente que del backend en Django (Sí, el backend
    de Django hace llamadas http al servidor geográfico para evitar problemas
    de dominios (crossdomain)), en el settings del proyecto se deben definir
    dos variables:
    
     * GEOSERVER_URL_CLIENT
     * GEOSERVER_URL
    
    El primero tiene la URL que usará el código Javascript para referirse
    al servidor Geográfico, y el segundo la URL que usará el código
    Python.
    
    Adicionalmente, se está preparando el terreno para utilizar la
    biblioteca fvislib, debido a la refactorización planificada de la
    función get_microzone_id, en la cuál se pretende definir una
    configuración que diga el nombre de las capas y su campo de interés.
    El campo de interés es en este caso específico el que tiene el
    nombre de la microzona.

--- espectros_microzonas/static/js/main.js
+++ espectros_microzonas/static/js/main.js
@@ -9,8 +9,7 @@
 // window.mapping namespace for the map-related functions.
 //
 
-server_ip = "190.73.8.234";
-geoserver_url = "http://"+ server_ip +":8080/geoserver/microzonas/wms";
+geoserver_url = configuration.microzonification.geoserver_url;
 
 window.mapping = {
     handler: function (e) {
