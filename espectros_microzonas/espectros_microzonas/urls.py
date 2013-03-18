0e74f955aec9ec781b087d3e6c692e55151c3fa6
Jesús Gómez <jgomo3@gmail.com>
2013-03-18 08:47:38 -0430

    Activa interfaz administrativa para la app microzonificacion

--- espectros_microzonas/espectros_microzonas/urls.py
+++ espectros_microzonas/espectros_microzonas/urls.py
@@ -3,8 +3,12 @@
 from django.conf.urls import patterns, include, url
 from apps.index.views import IndexView
 from apps.microzonificacion.views import MicrozoneDetail
+from django.contrib import admin
+
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view() ),
     url(r'^getmicrozone/', MicrozoneDetail.as_view(), name='microzone'),
+    (r'^admin/', include(admin.site.urls)),
 )
